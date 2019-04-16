2939372e2fde677d4a40942d19154ec56ffe32a3
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-04-16 11:03:32 +0700

    add icon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import PrimarySearchAppBar from "./PrimarySearchAppBar";
+
+import Airplane from '@material-ui/icons/AirplanemodeActive';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        open: false,
+    };
+
+    handleClickOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
+    render() {
+        return (
+            <div>
+                <PrimarySearchAppBar/>
+                <Airplane className="miring"/>
+            </div>
+        )
+    }
 }
 
 export default App;
